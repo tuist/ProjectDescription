7bf21502863f58d33798abbc9ada28e4210c8da7
Daniele Formichelli <df@bendingspoons.com>
2022-03-02 12:17:11 +0100

    feat: add 3.0.0 version

---
+++ Package.swift
@@ -0,0 +1,23 @@
+// swift-tools-version:5.5
+// The swift-tools-version declares the minimum version of Swift required to build this package.
+
+import PackageDescription
+
+let package = Package(
+    name: "ProjectDescription",
+    platforms: [.macOS(.v11)],
+    products: [
+        .library(
+            name: "ProjectDescription",
+            targets: ["ProjectDescription"]
+        )
+    ],
+    dependencies: [],
+    targets: [
+        .target(
+            name: "ProjectDescription",
+            dependencies: [],
+            path: "tuist/Sources/ProjectDescription"
+        ),
+    ]
+)
---
+++ README.md
@@ -0,0 +1,5 @@
+# ProjectAutomation
+
+This repository contains mirror for the [ProjectDescription](https://github.com/tuist/tuist/tree/main/Sources/ProjectDescription) framework in the tuist main repository.
+
+This enables users to add this framework to their `Package.swift` without having to download _all_ the dependencies the main repository has.
